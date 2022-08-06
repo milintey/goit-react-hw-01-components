8b2a6c3f78038451aec0f9b7512117b32263331d
Ilya Milinteev <ilyamilint@gmail.com>
2022-08-07 00:07:53 +0300

    comit2

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import { ProfileAvatar } from '../Profile/Profile.styled';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
     return (
-        <Box width="400px" border="normal" borderColor="text">
+        <Box width="300px" border="normal" borderColor="text">
             <Box display="flex" alignItems="center" flexDirection="column" justifyContent="center">
                 <ProfileAvatar
                 src={avatar}
@@ -16,16 +16,16 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
                 <p class="location">{location}</p>
             </Box>
 
-            <Box display="flex" alignItems="center" justifyContent="center" as="ul" bg="muted"  gridGap={6}>
-                <Box display="flex" alignItems="center" justifyContent="center" as="li" flexDirection="column">
+            <Box display="flex" alignItems="center" justifyContent="center" as="ul" bg="muted">
+                <Box display="flex" alignItems="center" justifyContent="center" as="li" flexDirection="column" width="100px" height="60px">
                     <span class="label">Followers</span>
                     <span class="quantity">{stats.followers}</span>
                 </Box>
-                <Box display="flex" alignItems="center" justifyContent="center" as="li" flexDirection="column">
+                <Box display="flex" alignItems="center" justifyContent="center" as="li" flexDirection="column" width="100px" height="60px">
                     <span class="label">Views</span>
                     <span class="quantity">{stats.views}</span>
                 </Box>
-                <Box display="flex" alignItems="center" justifyContent="center" as="li" flexDirection="column">
+                <Box display="flex" alignItems="center" justifyContent="center" as="li" flexDirection="column" width="100px" height="60px">
                     <span class="label">Likes</span>
                     <span class="quantity">{stats.likes}</span>
                 </Box>
